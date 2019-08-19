46d3af7bc1a7d346aa1f019ef6f2f44e1cebe9a4
Quix <nswells14@gmail.com>
2019-08-19 15:10:32 +0300

    Added Devise gem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,37 @@
 class ProjectsController < ApplicationController
 
+  def index
+    @projects = Project.all
+  end
+
   def new
   end
 
   def create
       @project = Project.new(project_params)
-      if @project.valid?
-        @project.save
+      if @project.save
+        redirect_to @project
       else
         render action: 'new'
       end
   end
 
+  def edit
+      @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+        redirect_to @project
+      else
+        render action: 'edit'
+      end
+  end
+
+  def destroy
+      
+  end
 
 
 
