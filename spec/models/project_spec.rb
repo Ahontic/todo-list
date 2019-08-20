8a2c70eaaf2c7e8276e43b316bd9684f5e21ad78
Quix <nswells14@gmail.com>
2019-08-20 22:41:32 +0300

    Added Project model test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   
   describe 'associations' do
-    it { should have_many(:task) }
+    it { should have_many(:tasks) }
   end
 
   describe "validations" do
