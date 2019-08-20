ca300b56a13c1bb3ec067512ce4f3b4c77623e1b
Quix <nswells14@gmail.com>
2019-08-21 00:43:58 +0300

    View files normalization

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :projects
+  resources :projects do
+    resources :tasks
+end
+
 end
