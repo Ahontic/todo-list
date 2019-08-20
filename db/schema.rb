b78a137c35a57c4344072ee45456ffc51022f5c1
Quix <nswells14@gmail.com>
2019-08-21 00:42:38 +0300

    Added user_id tag to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_125123) do
+ActiveRecord::Schema.define(version: 2019_08_20_212754) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
